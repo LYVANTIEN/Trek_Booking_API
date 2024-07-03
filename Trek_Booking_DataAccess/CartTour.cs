30b8491dbe8fdd27f53876878b6ad17658515a48
Tienvan2112 <55881057+LYVANTIEN@users.noreply.github.com>
2024-07-03 14:54:58 +0700

    new api

--- Trek_Booking_DataAccess/CartTour.cs
+++ Trek_Booking_DataAccess/CartTour.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
-namespace Trek_Booking_DataAccess
-{
-    [Table("CartTour")]
-    public class CartTour
+    namespace Trek_Booking_DataAccess
     {
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int CartTourId { get; set; }
-        [ForeignKey("Tour")]
-        public int TourId { get; set; }
-        public Tour? Tour { get; set; }
-        public int TourQuantity { get; set; }
-        public decimal TourPrice { get; set; }
-        [ForeignKey("User")]
-        public int UserId { get; set; }
-        public User? User { get; set; }
+        [Table("CartTour")]
+        public class CartTour
+        {
+            [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int CartTourId { get; set; }
+            [ForeignKey("Tour")]
+            public int TourId { get; set; }
+            public Tour? Tour { get; set; }
+            public int TourQuantity { get; set; }
+            public decimal TourPrice { get; set; }
+            [ForeignKey("User")]
+            public int UserId { get; set; }
+            public User? User { get; set; }
+        }
     }
-}
